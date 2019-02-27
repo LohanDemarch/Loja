d8c6d8d19b77cc6200b3d3338510638fcd7c6d0a
Lohan Gabriel Moraes Demarchch <lohan_demarch@hotmail.com>
2019-02-26 22:38:44 -0300

    Adicionado novo campo e melhorias
    Adicionado menu, e melhorado a "cara" do site.
    
    esta listando os produtos

---
+++ adiciona-produto.php
@@ -0,0 +1,26 @@
+<?php include ("cabecalho.php"); ?>
+<?php include ("conecta.php"); 
+
+
+
+$nome = $_GET['nome'];
+$preco = $_GET['preco'];
+
+function insereProduto($conexao, $nome, $preco) {
+    $query = "insert into produtos (nome, preco) values ('{$nome}', '{$preco}')";
+    $resultadoDaInsercao = mysqli_query($conexao, $query);
+    return $resultadoDaInsercao;
+}
+
+if(insereProduto($conexao, $nome, $preco)) {
+    ?>
+    <p class="alert-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
+    <?php
+} else {
+    ?>
+    <p class="alert-danger">O produto <? = $nome; ?> não foi adicionado</p>
+    <?php
+}
+?>
+
+<?php include ("rodape.php"); ?>
